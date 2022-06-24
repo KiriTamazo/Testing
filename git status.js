9935013c2b01d3092a5445aa80490bbe8bc35c96
KiriTamazo Htet Aung <kiritokirikaya80@gmail.com>
2022-06-24 15:08:14 +0630

    Update:All

--- git status.js
+++ git status.js
@@ -1,48 +1,10 @@
-[33mcommit 580d230ea70154aa7ecc4964f3de94af32ebc1fa[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
-Author: Kirito <kiritokirikaya80@gmail.com>
-Date:   Sat Jun 11 22:31:10 2022 +0630
-
-    Commit again
-
-[33mcommit 80b34d15fafc3781eb68f3722e1590ec9800cd45[m
-Author: Kirito <kiritokirikaya80@gmail.com>
-Date:   Sat Jun 11 22:16:03 2022 +0630
-
-    Commit
-
-[33mcommit 10ad48958c385f4f0f99a321161da742ff0c67b4[m
-Author: Kirito <kiritokirikaya80@gmail.com>
-Date:   Sat Jun 11 22:09:20 2022 +0630
-
-    Initial Commit
-
-[33mcommit 51a8cff56393e4b004058ac97b506d7aa6c7cd91[m[33m ([m[1;31morigin/Testing[m[33m)[m
-Merge: 06fa6be b878c14
-Author: KiriTamazo <kiritokirikaya80@gmail.com>
-Date:   Sat Jun 11 21:30:07 2022 +0630
-
-    Merge branch 'main' of https://github.com/KiriTamazo/Testing
-
-[33mcommit b878c14a8e4f79723102aafe0a95b8126bc7474e[m
-Author: La Woon Htet Aung <65594552+KiriTamazo@users.noreply.github.com>
-Date:   Sat Jun 11 17:54:49 2022 +0630
-
-    Create README.md
-
-[33mcommit 87aa13c85ad7a71ab77ae190cbdf9e73f251f4bf[m
-Author: La Woon Htet Aung <65594552+KiriTamazo@users.noreply.github.com>
-Date:   Sat Jun 11 17:54:39 2022 +0630
-
-    Delete README.md
-
-[33mcommit 06fa6be445cc25d72c611ca8cc402525a2652e2d[m
-Author: KiriTamazo <kiritokirikaya80@gmail.com>
-Date:   Sat Jun 11 17:51:24 2022 +0630
-
-    commit
-
-[33mcommit 910dd4759172c3fa728fb806a7b9ec1e9b5f9eb5[m
-Author: KiriTamazo <kiritokirikaya80@gmail.com>
-Date:   Sat Jun 11 17:41:31 2022 +0630
-
-    first commit
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body></body>
+</html>
